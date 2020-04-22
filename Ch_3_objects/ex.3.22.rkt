436d63fb63b2a93e6117cce3c73ddcde9e2ec14b
NickSuh <hyoungmin.suh@student.kuleuven.be>
2020-04-22 08:15:24 +0200

    ex.3.22

---
+++ Ch_3_objects/ex.3.22.rkt
@@ -0,0 +1,49 @@
+#lang sicp
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)(else
+                  (set-cdr! (rear-ptr queue) new-pair)
+                  (set-rear-ptr! queue new-pair)
+                  queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (cdr (front-ptr queue)))
+              queue)))
+
+(define (make-queue1)
+  (let ((front-ptr '())
+        (rear-ptr '() ))
+        (define (dispatch m)
+          (cond ( (eq? m 'car) front-ptr)
+                ( (eq? m 'cdr) rear-ptr)
+                ( (eq? m 'set-car) (lambda (x) (set! front-ptr x)))
+                ( (eq? m 'set-cdr) (lambda (x) (set! rear-ptr x)))))
+        dispatch))
+
+(define myqueue (make-queue1))
+
+((myqueue 'set-car) 'hii)
+
+(myqueue 'car)
+                                   
+        
